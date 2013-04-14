f4b505d4b048e154947b209a426256221c4834cd
akiniwa <shin.akiba8@gmail.com>
2013-04-15 00:11:30 +0900

    Scrapy tutorial project worked

---
+++ first_proj/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for first_proj project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'first_proj'
+
+SPIDER_MODULES = ['first_proj.spiders']
+NEWSPIDER_MODULE = 'first_proj.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'first_proj (+http://www.yourdomain.com)'
